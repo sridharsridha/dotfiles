287634eb99d22b284ddbd6ab6d71f24d4981b65d
sridharsridha <sridha.in@gmail.com>
2022-02-09 13:01:49 +0530

    Fzf removed custom changes

--- bash/bash/custom/fzf.bash
+++ bash/bash/custom/fzf.bash
@@ -1,11 +1,11 @@
 # Color: https://github.com/junegunn/fzf/wiki/Color-schemes - Solarized Dark
 # Bind F1 key to toggle preview window on/off
-export FZF_DEFAULT_OPTS='--bind "F1:toggle-preview" --preview "rougify {} 2> /dev/null || cat {} 2> /dev/null || tree -C {} 2> /dev/null | head -100" --color dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254 --color info:254,prompt:37,spinner:108,pointer:235,marker:235'
+# export FZF_DEFAULT_OPTS='--bind "F1:toggle-preview" --preview "rougify {} 2> /dev/null || cat {} 2> /dev/null || tree -C {} 2> /dev/null | head -100" --color dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254 --color info:254,prompt:37,spinner:108,pointer:235,marker:235'
 
 # Show long commands if needed
 # From https://github.com/junegunn/fzf/wiki/Configuring-shell-key-bindings
 # Bind F1 key to toggle preview window on/off
-export FZF_CTRL_R_OPTS='--bind "F1:toggle-preview" --preview "echo {}" --preview-window down:3:hidden:wrap'
+# export FZF_CTRL_R_OPTS='--bind "F1:toggle-preview" --preview "echo {}" --preview-window down:3:hidden:wrap'
 
 # FZF installation
 [ ! -f ~/.fzf.bash ] && $BASH_D/fzf/install --all
