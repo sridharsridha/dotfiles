6863d97b7cfe48b35f128d6147742d65cb77b98f
sridharsridha <sridha.in@gmail.com>
2022-08-11 13:31:59 +0530

    nit

--- install.sh
+++ install.sh
@@ -16,17 +16,17 @@ git submodule update --init --recursive
 # Install Fonts
 if [[ -d ~/Library/Fonts/ ]]; then
     echo "Installing fonts..."
-	 echo sudo cp ${PWD}/fonts/* ~/Library/Fonts/
-    sudo cp ${PWD}/fonts/* ~/Library/Fonts/ || true
+	 echo sudo cp $PWD/fonts/* ~/Library/Fonts/
+    sudo cp $PWD/fonts/* ~/Library/Fonts/ || true
 fi
 
 # ZSH
 if command -v zsh &> /dev/null; then
 	echo "Installing zsh dotfiles..."
-	echo ln -s ${PWD}/zsh ~/.zsh
-	ln -s ${PWD}/zsh ~/.zsh || true
-	echo ln -s ${PWD}/zshrc ~/.zshrc
-	ln -s ${PWD}/zshrc ~/.zshrc || true
+	echo ln -s $PWD/zsh ~/.zsh
+	ln -s $PWD/zsh ~/.zsh || true
+	echo ln -s $PWD/zshrc ~/.zshrc
+	ln -s $PWD/zshrc ~/.zshrc || true
 fi
 
 if command -v bash &> /dev/null; then
@@ -42,23 +42,23 @@ fi
 # Tmux
 if command -v tmux &> /dev/null; then
 	echo "Installing tmux dotfiles..."
-	echo ln -s ${PWD}/tmux.conf ~/.tmux.conf
-	ln -s ${PWD}/tmux.conf ~/.tmux.conf || true
+	echo ln -s $PWD/tmux.conf ~/.tmux.conf
+	ln -s $PWD/tmux.conf ~/.tmux.conf || true
 fi
 
 # Neovim
 if command -v nvim &> /dev/null; then
 	echo "Installing neovim dotfiles..."
-	echo ln -s ${PWD}/nvim ~/.config/nvim
-	ln -s ${PWD}/nvim ~/.config/nvim || true
+	echo ln -s $PWD/nvim ~/.config/nvim
+	ln -s $PWD/nvim ~/.config/nvim || true
 fi
 
 # iTerm2 installation
 if [[ -d /Applications/iTerm.app ]]; then
 	echo "Installing iTerm2 dotfiles..."
 	# Specify the preferences directory to iTerm2
-	echo defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string ${PWD}
-	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string ${PWD}/iterm || true
+	echo defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string $PWD
+	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string $PWD/iterm || true
 	# Tell iTerm2 to use the custom preferences in the directory
 	echo defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 	defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true || true
@@ -67,11 +67,11 @@ fi
 # Install git config
 if command -v git &> /dev/null; then
 	echo "Installing git dotfiles..."
-	echo ln -s ${PWD}/git/gitconfig ~/.gitconfig
-	ln -s ${PWD}/git/gitconfig ~/.gitconfig || true
-	echo ln -s ${PWD}/git/gitignore ~/.gitignore
-	ln -s ${PWD}/git/gitignore ~/.gitignore || true
-	echo ln -s ${PWD}/git/gitattributes ~/.gitattributes
-	ln -s ${PWD}/git/gitattributes ~/.gitattributes || true
+	echo ln -s $PWD/git/gitconfig ~/.gitconfig
+	ln -s $PWD/git/gitconfig ~/.gitconfig || true
+	echo ln -s $PWD/git/gitignore ~/.gitignore
+	ln -s $PWD/git/gitignore ~/.gitignore || true
+	echo ln -s $PWD/git/gitattributes ~/.gitattributes
+	ln -s $PWD/git/gitattributes ~/.gitattributes || true
 fi
 
