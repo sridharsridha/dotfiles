6c7ea429cc25fd8aa1860715792e767bdde9e27b
sridharsridha <sridha.in@gmail.com>
2024-08-27 01:38:46 +0530

    improve startuptime

--- nvim/lua/sri/plugins/git.lua
+++ nvim/lua/sri/plugins/git.lua
@@ -1,24 +1,36 @@
 return {
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		cmd = "Gitsigns",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "+" },
-					change = { text = "~" },
-					delete = { text = "_" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "│" },
-				},
-				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
-					virt_text = true,
-				},
-			})
-		end,
-	},
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
-	{ "sindrets/diffview.nvim", opts = {} },
+   { -- Adds git related signs to the gutter, as well as utilities for managing changes
+      "lewis6991/gitsigns.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      cmd = "Gitsigns",
+      config = function()
+         require("gitsigns").setup({
+            signs = {
+               add = { text = "+" },
+               change = { text = "~" },
+               delete = { text = "_" },
+               topdelete = { text = "‾" },
+               changedelete = { text = "│" },
+            },
+            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+               virt_text = true,
+            },
+         })
+      end,
+   },
+   {
+      "akinsho/git-conflict.nvim",
+      opt = {
+         default_mappings = true,     -- disable buffer local mapping created by this plugin
+         default_commands = true,     -- disable commands created by this plugin
+         disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+         list_opener = 'copen',       -- command or function to open the conflicts list
+         highlights = {               -- They must have background color, otherwise the default color will be used
+            incoming = 'DiffAdd',
+            current = 'DiffChange',
+         }
+      }
+   },
+   { "sindrets/diffview.nvim", cmd = "DiffviewOpen", opts = {} },
 }
--- nvim/lua/sri/plugins/ui.lua
+++ nvim/lua/sri/plugins/ui.lua
@@ -1,143 +1,160 @@
 return {
-	{ "echasnovski/mini.tabline", opts = {} },
+   { "echasnovski/mini.tabline",    opts = {} },
 
-	{ "echasnovski/mini.statusline", opts = { use_icons = false } },
+   { "echasnovski/mini.statusline", opts = { use_icons = false } },
 
-	{
-		"echasnovski/mini.indentscope",
-		keys = { "j", "k" },
-		opts = function(_, opts)
-			opts.draw = {
-				animation = require("mini.indentscope").gen_animation.none(),
-			}
-		end,
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"alpha",
-					"dashboard",
-					"help",
-					"lazy",
-					"lazyterm",
-					"man",
-					"mason",
-					"neo-tree",
-					"notify",
-					"Outline",
-					"toggleterm",
-					"Trouble",
-					"lspinfo",
-				},
-				callback = function()
-					vim.b["miniindentscope_disable"] = true
-				end,
-			})
-		end,
-	},
+   {
+      "echasnovski/mini.indentscope",
+      keys = { "j", "k" },
+      opts = function(_, opts)
+         opts.draw = {
+            animation = require("mini.indentscope").gen_animation.none(),
+         }
+      end,
+      init = function()
+         vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+               "alpha",
+               "dashboard",
+               "help",
+               "lazy",
+               "lazyterm",
+               "man",
+               "mason",
+               "neo-tree",
+               "notify",
+               "Outline",
+               "toggleterm",
+               "Trouble",
+               "lspinfo",
+            },
+            callback = function()
+               vim.b["miniindentscope_disable"] = true
+            end,
+         })
+      end,
+   },
 
-	-- Better quickfix window in Neovim
-	{
-		"kevinhwang91/nvim-bqf",
-		ft = "qf",
-		cmd = "BqfAutoToggle",
-		event = "QuickFixCmdPost",
-		opts = {
-			auto_resize_height = false,
-			func_map = {
-				tab = "st",
-				split = "sh",
-				vsplit = "sv",
+   -- Better quickfix window in Neovim
+   {
+      "kevinhwang91/nvim-bqf",
+      ft = "qf",
+      cmd = "BqfAutoToggle",
+      event = "QuickFixCmdPost",
+      opts = {
+         auto_resize_height = false,
+         func_map = {
+            tab = "st",
+            split = "sh",
+            vsplit = "sv",
 
-				stoggleup = "K",
-				stoggledown = "J",
+            stoggleup = "K",
+            stoggledown = "J",
 
-				ptoggleitem = "p",
-				ptoggleauto = "P",
-				ptogglemode = "zp",
+            ptoggleitem = "p",
+            ptoggleauto = "P",
+            ptogglemode = "zp",
 
-				pscrollup = "<C-b>",
-				pscrolldown = "<C-f>",
+            pscrollup = "<C-b>",
+            pscrolldown = "<C-f>",
 
-				prevfile = "gk",
-				nextfile = "gj",
+            prevfile = "gk",
+            nextfile = "gj",
 
-				prevhist = "<S-Tab>",
-				nexthist = "<Tab>",
-			},
-			preview = {
-				auto_preview = true,
-				should_preview_cb = function(bufnr)
-					-- file size greater than 100kb can't be previewed automatically
-					local filename = vim.api.nvim_buf_get_name(bufnr)
-					local fsize = vim.fn.getfsize(filename)
-					if fsize > 100 * 1024 then
-						return false
-					end
-					return true
-				end,
-			},
-		},
-	},
+            prevhist = "<S-Tab>",
+            nexthist = "<Tab>",
+         },
+         preview = {
+            auto_preview = true,
+            should_preview_cb = function(bufnr)
+               -- file size greater than 100kb can't be previewed automatically
+               local filename = vim.api.nvim_buf_get_name(bufnr)
+               local fsize = vim.fn.getfsize(filename)
+               if fsize > 100 * 1024 then
+                  return false
+               end
+               return true
+            end,
+         },
+      },
+   },
 
-	{
-		"kevinhwang91/nvim-hlslens",
-		opts = {
-			calm_down = true,
-		},
-		keys = {
-			-- FYI: debug mapping with `:map ...`
-			{
-				"n",
-				[[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
-				noremap = true,
-				silent = true,
-				desc = "Next Match",
-			},
-			{
-				"N",
-				[[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
-				noremap = true,
-				silent = true,
-				desc = "Previous Match",
-			},
+   {
+      "yorickpeterse/nvim-pqf",
+      ft = "qf",
+      event = "QuickFixCmdPost",
+      opt = {
+         signs = {
+            error = { text = '', hl = 'DiagnosticSignError' },
+            warning = { text = '', hl = 'DiagnosticSignWarn' },
+            info = { text = '', hl = 'DiagnosticSignInfo' },
+            hint = { text = '󰌵', hl = 'DiagnosticSignHint' },
+         },
+      }
+   },
 
-			-- TODO: Respect smartcase with:
-			--  https://github.com/olimorris/dotfiles-1/blob/0a3168e068e21fd9f51be27fe7bdb72ef2643d88/.config/nvim/lua/plugins/hlslens.lua#L11-L31
-			{ "*", [[*<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
-			{ "#", [[#<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
-			{ "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
-			{ "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
-		},
-	},
+   {
+      "kevinhwang91/nvim-hlslens",
+      opts = {
+         calm_down = true,
+      },
+      keys = {
+         -- FYI: debug mapping with `:map ...`
+         {
+            "n",
+            [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+            noremap = true,
+            silent = true,
+            desc = "Next Match",
+         },
+         {
+            "N",
+            [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+            noremap = true,
+            silent = true,
+            desc = "Previous Match",
+         },
 
-	{ "max397574/better-escape.nvim", opts = {
-		timeout = vim.o.timeoutlen,
-		default_mappings = true,
-	} },
+         -- TODO: Respect smartcase with:
+         --  https://github.com/olimorris/dotfiles-1/blob/0a3168e068e21fd9f51be27fe7bdb72ef2643d88/.config/nvim/lua/plugins/hlslens.lua#L11-L31
+         { "*",  [[*<Cmd>lua require('hlslens').start()<CR>]],  noremap = true, silent = true, desc = "Match Word" },
+         { "#",  [[#<Cmd>lua require('hlslens').start()<CR>]],  noremap = true, silent = true, desc = "Match Word" },
+         { "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
+         { "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], noremap = true, silent = true, desc = "Match Word" },
+      },
+   },
 
-	{
-		"nacro90/numb.nvim",
-		event = { "CmdlineEnter" },
-		config = function()
-			require("numb").setup()
-		end,
-	},
+   {
+      "max397574/better-escape.nvim",
+      opts = {
+         timeout = vim.o.timeoutlen,
+         default_mappings = true,
+      }
+   },
 
-	{
-		"winston0410/range-highlight.nvim",
-		dependencies = { "winston0410/cmd-parser.nvim" },
-		config = true,
-	},
+   {
+      "nacro90/numb.nvim",
+      event = { "CmdlineEnter" },
+      config = function()
+         require("numb").setup()
+      end,
+   },
 
-	{
-		"kawre/neotab.nvim",
-		event = "InsertEnter",
-		opts = {},
-	},
+   -- {
+   -- 	"winston0410/range-highlight.nvim",
+   -- 	dependencies = { "winston0410/cmd-parser.nvim" },
+   -- 	config = true,
+   -- },
 
-	{
-		"AckslD/muren.nvim",
-		cmd = { "MurenOpen", "MurenClose", "MurenToggle" },
-		config = true,
-	},
+   {
+      "kawre/neotab.nvim",
+      event = "InsertEnter",
+      opts = {},
+   },
+
+   {
+      "AckslD/muren.nvim",
+      cmd = { "MurenOpen", "MurenClose", "MurenToggle" },
+      config = true,
+   },
 }
