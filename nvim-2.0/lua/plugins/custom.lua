3dfbadb9685bc147359ca4ba47d15a9eef8596ee
sridharsridha <sridha.in@gmail.com>
2024-12-26 22:41:33 +0530

    Fixed custom

--- nvim-2.0/lua/plugins/custom.lua
+++ nvim-2.0/lua/plugins/custom.lua
@@ -3,7 +3,7 @@ return {
 		dir = "~/vim-plugins/arista.nvim",
 		lazy = false,
 		enabled = function()
-			return (vim.uv or vim.loop).fs_stat("~/vim-plugins/arista.nvim")
+			return vim.uv.fs_stat(vim.fn.expand("$HOME/vim-plugins/arista.nvim"))
 		end,
 	},
 }
