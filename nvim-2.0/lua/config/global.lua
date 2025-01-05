6ed0cdfb209595ec767f1535965b37245dac857d
sridharsridha <sridha.in@gmail.com>
2025-01-05 12:49:27 +0530

    Added global options

--- nvim-2.0/lua/config/global.lua
+++ nvim-2.0/lua/config/global.lua
@@ -3,5 +3,10 @@ local M = {}
 --- Root patterns for auto detect and change root
 ---@type string[]
 M.root_patterns = { ".git", "BUILD.qb" }
+M.mapleader = " "
+M.maplocalleader = ","
+M.netrw_silent = 1
+M.disable_autoformat = false
+M.use_icons = true
 
 return M
