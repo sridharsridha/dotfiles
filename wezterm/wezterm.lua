5241c4eee76e0d35c5e8e520297e18f80a23bd06
sridharsridha <sridha.in@gmail.com>
2025-01-11 20:56:41 +0530

    wezterm and ghostty background opacity changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,7 +17,7 @@ config.font_size = 13
 
 -- Window
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 20
 
 -- Tabs
