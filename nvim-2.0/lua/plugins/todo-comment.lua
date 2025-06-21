f59c54c8e00852e20d31f139be18a087089e9243
sridharsridha <sridha.in@gmail.com>
2025-06-21 08:09:45 +0530

    updated todo to be lazy

--- nvim-2.0/lua/plugins/todo-comment.lua
+++ nvim-2.0/lua/plugins/todo-comment.lua
@@ -1,13 +1,28 @@
 return {
 	{
 		"folke/todo-comments.nvim",
-      event = "VeryLazy",
+      event = { "BufReadPost", "BufNewFile" },
+		cmd = { "TodoTrouble", "TodoTelescope", "TodoLocList", "TodoQuickfix" }, -- Load only when these commands are run
+		keys = {
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next Todo Comment",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Previous Todo Comment",
+			},
+			{ "<leader>ft", "<cmd>TodoTrouble<cr>", desc = "Find Todo (Trouble)" },
+		},
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+         signs = true,
 		},
 	},
 }
-
