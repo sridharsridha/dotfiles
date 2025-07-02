d66dd2a251e564785a86172399b76b6e17a33d13
sridharsridha <sridha.in@gmail.com>
2025-07-03 00:15:09 +0530

    wezterm changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,7 +17,7 @@ config.font_size = 13
 
 -- Window
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 
 -- Tabs
