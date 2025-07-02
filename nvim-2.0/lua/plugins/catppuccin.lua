cb127870e5a2a5c76c496e7d013302bab34ed8df
sridharsridha <sridha.in@gmail.com>
2025-07-03 00:14:43 +0530

    Tmxu changes

--- nvim-2.0/lua/plugins/catppuccin.lua
+++ nvim-2.0/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha",
-				transparent_background = false,
+				transparent_background = true,
 			})
 			vim.cmd.colorscheme("catppuccin")
 		end,
